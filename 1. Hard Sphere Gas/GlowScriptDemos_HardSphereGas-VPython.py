7f1c6ff416b9589590ca8dd762236bb9ebae55fc
davidrosamolina <davidromo872004@gmail.com>
2025-04-15 13:05:45 +0200

    Update GlowScriptDemos_HardSphereGas-VPython.py

--- 1. Hard Sphere Gas/GlowScriptDemos_HardSphereGas-VPython.py
+++ 1. Hard Sphere Gas/GlowScriptDemos_HardSphereGas-VPython.py
@@ -185,7 +185,7 @@ while True:
         interchange(vj.mag, p[j].mag/mass)
 
         colisions += 1
-        if colisions % 10000 == 0:    #cada 10000 colisions mirem el temps mig
+        if colisions % 5000 == 0:    #cada 5000 colisions mirem el temps mig
             print("Nombre de colisions:", colisions)
             print("Temps total:", temps_total)
             print("Tiemps mig entre colisions:", temps_total / colisions)
