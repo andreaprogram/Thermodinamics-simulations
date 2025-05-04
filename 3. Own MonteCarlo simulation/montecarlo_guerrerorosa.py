18ff52c0cf9d94c2b3c09bea08251e6e0a403cbb
andreaprogram <55930694+andreaprogram@users.noreply.github.com>
2025-05-04 19:31:33 +0200

    Update montecarlo_guerrerorosa.py

--- 3. Own MonteCarlo simulation/montecarlo_guerrerorosa.py
+++ 3. Own MonteCarlo simulation/montecarlo_guerrerorosa.py
@@ -8,22 +8,22 @@ Created on Sun May  4 13:11:04 2025
 import numpy as np
 import matplotlib.pyplot as plt
 
-# VARIABLES TERMODINÀMIQUES FIXADES
+# VARIABLES TERMODINAMIQUES FIXADES
 
 N=1000 #nombre de mol.lecules
 V=1 #volum [m^3]
 T=293 #temperatura [K]
-k=1.4E-23 [J/K]
+k=1.4E-23 #constant de Boltzmann [J/K]
 
 
-print("Indicate the dimension in which you want the system (1, 2 or 3)")
+print("Indiqueu la dimensio en que voleu el sistema (1, 2 o 3)")
 while True:
     d = int(input())
     if d==1 or d==1 or d==3:
-        print("The dimension chosen is d =", d)
+        print("La dimensio escollida es d =", d)
         break
     else:
-        print("Please enter only 1, 2 or 3")
+        print("Si us plau introdueix unicament 1, 2 o 3")
 
 C_v_teo=d*N*k/2 #calculem la capacitat teòrica
 
