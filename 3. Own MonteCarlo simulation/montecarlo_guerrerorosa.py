a5093a96401d021c0627a983ebaa017f3b29f201
andreaprogram <55930694+andreaprogram@users.noreply.github.com>
2025-05-04 20:48:45 +0200

    Update montecarlo_guerrerorosa.py

--- 3. Own MonteCarlo simulation/montecarlo_guerrerorosa.py
+++ 3. Own MonteCarlo simulation/montecarlo_guerrerorosa.py
@@ -14,9 +14,17 @@ N=1000 #nombre de mol.lecules
 V=1 #volum [m^3]
 T=293 #temperatura [K]
 kb=1.4E-23 #constant de Boltzmann [J/K]
-k=1 #constant del potencial harmonic
+k=1 #constant del potencial harmonic E = 1/2*k*r^2
+L=1 #llargada de la recta / costat del pla / aresta de la caixa on son els atoms
 
+n_p = 100 #nombre de passos de la simulacio
 
+def E(r): #potencial harmonic
+    return 0.5 * k * np.sum(r**2)
+
+def Cv(d):
+    return d*N*k/2 #calculem la capacitat teorica
+    
 #DEMANAR LA DIMENSIO--------------------------------------------------------------
 print("Indiqueu la dimensio en que voleu el sistema (1, 2 o 3)")
 while True:
@@ -28,8 +36,15 @@ while True:
         print("Si us plau introdueix unicament 1, 2 o 3")
 
 
-C_v_teo=d*N*k/2 #calculem la capacitat teorica
-def E(r):
-    return 0.5 * k * np.sum(r**2)
+
+
+#BUCLE QUE FA LA SIMULACIO---------------------------------------------------
+r = np.random.uniform(-L, L, size=(N, d)) #llista on s'emmagatzemmen les posicions de les N particules en d dimensions contingudes en la longitud L
+
+for pas in range(n_p):
+    for i in range(N):
+        r[i]=np.
+
+
 
     
