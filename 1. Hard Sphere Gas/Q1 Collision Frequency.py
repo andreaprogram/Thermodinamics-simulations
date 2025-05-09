709d687e3a8078fe98e9c5e56347e2a624be8e3b
davidrosamolina <davidromo872004@gmail.com>
2025-05-09 11:14:23 +0200

    Update Q1 Collision Frequency.py

--- 1. Hard Sphere Gas/Q1 Collision Frequency.py
+++ 1. Hard Sphere Gas/Q1 Collision Frequency.py
@@ -191,7 +191,7 @@ while True:
         interchange(vj.mag, p[j].mag/mass)
 
         colisions += 1
-        if colisions % 5000 == 0:    #cada 5000 colisions mirem el temps mig
+      if colisions % 5000 == 0:    #cada 5000 colisions mirem el temps mig
             print("Nombre de colisions:", colisions)
             print("Temps total:", temps_total)
             print("Tiemps mig entre colisions:", temps_total / colisions)
